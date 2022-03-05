683c0c1101c53559e8916a287d9d772d98b20de4
keanu@mesmerise.it <keanu@mesmerise.it>
2022-03-05 22:09:35 +0000

    OldMapGenerator import.

---
+++ Assets/Scripts/Utils/InspectorButtonAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class InspectorButtonAttribute : PropertyAttribute
+{
+    public bool PlayModeOnly { get; private set; }
+    public bool IsHoldButton { get; private set; }
+    public string TextStr { get; private set; }
+
+    public InspectorButtonAttribute() : this(false, false) { }
+    public InspectorButtonAttribute(bool playModeOnly, bool isHoldButton, string textStr = null)
+    {
+        PlayModeOnly = playModeOnly;
+        IsHoldButton = isHoldButton;
+        TextStr = textStr;
+    }
+}
