808582350a74c811b58aa79294f6db3b4ac06091
keanu@mesmerise.it <keanu@mesmerise.it>
2022-03-18 06:41:00 +0000

    minor

--- Assets/Scripts/Directors/StageDirector.cs
+++ Assets/Scripts/Directors/StageDirector.cs
@@ -1,10 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
-
 using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
-using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
 public class StageDirector : Director
